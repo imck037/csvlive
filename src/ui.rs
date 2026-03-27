ddd56330e7ef51f3dc1eeecdb40af265dcc2c293
imck037 <chandradhar.2003@gmail.com>
2026-03-27 21:10:43 +0530

    status bar is added for both modes

--- src/ui.rs
+++ src/ui.rs
@@ -2,17 +2,17 @@ use ratatui::{
     Frame,
     layout::{Constraint, Direction, Layout},
     style::{Color, Style},
-    widgets::{Block, Borders, Cell, Row, Table},
+    widgets::{Block, Borders, Cell, Paragraph, Row, Table},
 };
 
-use crate::App;
+use crate::{App, Mode};
 
 pub fn render_ui(frame: &mut Frame, app: &App) {
     let area = frame.area();
 
     let layout = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(100)])
+        .constraints([Constraint::Min(10), Constraint::Length(2)])
         .split(area);
 
     let rows = app.contents.iter().enumerate().map(|(i, row)| {
@@ -26,7 +26,26 @@ pub fn render_ui(frame: &mut Frame, app: &App) {
         Row::new(cells)
     });
 
-    let table = Table::new(rows, [Constraint::Percentage(10); 10])
-        .block(Block::default().title("CSV Live").borders(Borders::all()));
+    let widths: Vec<Constraint> = app.contents[0]
+        .iter()
+        .map(|_| Constraint::Percentage(100 / app.contents[0].len() as u16))
+        .collect();
+
+    let table =
+        Table::new(rows, widths).block(Block::default().title("CSV Live").borders(Borders::all()));
     frame.render_widget(table, layout[0]);
+
+    let status_bar = match app.mode {
+        Mode::Normal => String::from("Normal i: insert mode"),
+        Mode::Insert => String::from("Insert ESC: cancel edit, <CR>: save edit"),
+    };
+
+    let input_bar = match app.mode {
+        Mode::Normal => String::from("j: down, k: up, l: right, h: left"),
+        Mode::Insert => String::from(format!("Edit: {}", app.input)),
+    };
+
+    let status: Vec<String> = vec![status_bar, input_bar];
+
+    frame.render_widget(Paragraph::new(status.join("\n")), layout[1]);
 }
